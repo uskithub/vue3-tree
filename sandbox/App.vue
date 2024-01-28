d2a0a7bf6efd2d76159fdaacc971df22a8c71947
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2024-01-28 19:25:32 +0900

    no message

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -44,39 +44,103 @@ const vFocus = {
 
 const treeContent = {
   id: "0"
-  , title: "root1"
+  , title: "Vue3-treeの動作を確認する"
   , children: [
     {
       id: "1"
-      , title: "subtree1"
+      , title: "初期表示が正しいこと"
       , children: [
         {
           id: "11"
-          , title: "subtree1-1"
+          , title: "ツリーすべてが表示されること"
           , children: [] as MyContent[]
         } as MyContent
         , {
           id: "12"
-          , title: "subtree1-2"
-          , children: [
-            {
-              id: "121"
-              , title: "subtree1-2-1"
-              , children: [] as MyContent[]
-            } as MyContent
-          ]
+          , title: "階層構造に間違いがないこと"
+          , children: [] as MyContent[]
+        } as MyContent
+        , {
+          id: "13"
+          , title: "キャレットの開閉が正しいこと"
+          , children: [] as MyContent[]
         } as MyContent
       ]
     } as MyContent
     , {
       id: "2"
-      , title: "subtree2"
-      , children: [] as MyContent[]
+      , title: "アクションによる表示の変化が正しくされること"
+      , children: [
+          {
+            id: "21"
+            , title: "キャレットが正しく切り替わること"
+            , children: [
+              {
+                id: "211"
+                , title: "開いているときにクリックで閉じること"
+                , children: [] as MyContent[]
+              } as MyContent
+              , {
+                id: "212"
+                , title: "閉じているときにクリックで開くこと"
+                , children: [] as MyContent[]
+              } as MyContent
+            ]
+          } as MyContent
+          , {
+              id: "22"
+              , title: "ドラッグが正しく動作すること"
+              , children: [
+                {
+                  id: "221"
+                  , title: "ドラッグすると元と同様の要素が半透明で表示されること"
+                  , children: [] as MyContent[]
+                } as MyContent
+                , {
+                  id: "222"
+                  , title: "ドロップターゲット（ピンク）が正しく表示されること"
+                  , children: [
+                      {
+                        id: "2221"
+                        , title: "ドラッグが他要素の子領域に入ったとき、ドロップターゲットが表示されること"
+                        , children: [] as MyContent[]
+                      }
+                      , {
+                        id: "2222"
+                        , title: "ドロップターゲット表示中、ドラッグが別の要素の子領域に入ると、ドロップターゲットが更新されること"
+                        , children: [] as MyContent[]
+                      }
+                      , {
+                        id: "2223"
+                        , title: "ドロップターゲット表示中、ドラッグが元の領域に入ると、ドロップターゲットが消えること"
+                        , children: [] as MyContent[]
+                      }
+                    ]
+                } as MyContent
+              ]
+            } as MyContent
+      ]
     } as MyContent
     , {
       id: "3"
-      , title: "subtree3"
-      , children: [] as MyContent[]
+      , title: "表示通りにノードの更新が行われていること"
+      , children: [
+          {
+            id: "31"
+            , title: "ifFolfing（折りたたまれている）が更新されること"
+            , children: [] as MyContent[]
+          } as MyContent
+          , {
+            id: "32"
+            , title: "node.nameが更新されること"
+            , children: [] as MyContent[]
+          } as MyContent
+          , {
+            id: "33"
+            , title: "tree内のノードの移動が更新されること"
+            , children: [] as MyContent[]
+          } as MyContent
+      ] 
     } as MyContent
   ]
 } as MyContent;
