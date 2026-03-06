9817b5f0fb6c80d50146a7e9552f783f2f951899
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2026-03-07 08:55:49 +0900

    Add missing Vuetify configuration to the sandbox

--- sandbox/main.ts
+++ sandbox/main.ts
@@ -2,6 +2,27 @@ import App from "./App.vue";
 import { createApp } from "vue";
 import Vue3TreePlugin from "../src";
 
+import "@mdi/font/css/materialdesignicons.css"
+
+import { createVuetify } from "vuetify"
+
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+import { aliases, mdi } from "vuetify/iconsets/mdi"
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+            mdi
+        }
+    }    
+})
+
 const app = createApp(App);
+app.use(vuetify);
 app.use(Vue3TreePlugin);
 app.mount("#app");
